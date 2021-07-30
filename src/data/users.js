3a1b96c9e8d1ddd366f4a23f574198bcc2772c89
Mauro Nunes <maurao.dev@gmail.com>
2021-07-30 17:05:13 -0300

    Listagem de usuários

---
+++ src/data/users.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    name: 'Mauro Nunes',
+    email: 'maurao.dev@gmail.com',
+    avatarUrl:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+  },
+  {
+    id: 2,
+    name: 'Poli',
+    email: 'maurao.dev@gmail.com',
+    avatarUrl:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+  },
+  {
+    id: 3,
+    name: 'Elena',
+    email: 'maurao.dev@gmail.com',
+    avatarUrl:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+  },
+];
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,4 +1,31 @@
-import React from "react";
-import { Text } from "react-native";
+import React from 'react';
+import { View, FlatList } from 'react-native';
+import { ListItem, Avatar } from 'react-native-elements';
+import users from '../data/users';
 
-export default props => <Text>User List</Text>;
+export default (props) => {
+  function getUserItem({ item: user }) {
+    return (
+      <ListItem
+        key={user.id}
+        bottomDivider
+        onPress={() => props.navigation.navigate('UserForm')}>
+        <Avatar source={{ uri: user.avatarUrl }} />
+        <ListItem.Content>
+          <ListItem.Title>{user.name}</ListItem.Title>
+          <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    );
+  }
+
+  return (
+    <View>
+      <FlatList
+        keyExtractor={(user) => user.id.toString()}
+        data={users}
+        renderItem={getUserItem}
+      />
+    </View>
+  );
+};
