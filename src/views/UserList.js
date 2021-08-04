832e242b97bf02853febf505dabf81f78add4387
Mauro Nunes <maurao.dev@gmail.com>
2021-08-03 23:48:53 -0300

    List 2

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,20 +1,55 @@
 import React from 'react';
-import { View, FlatList } from 'react-native';
+import { View, FlatList, Alert, Icon } from 'react-native';
 import { ListItem, Avatar } from 'react-native-elements';
+import { Button } from 'react-native-elements/dist/buttons/Button';
 import users from '../data/users';
 
 export default (props) => {
+  function confirmUserDeletion(user) {
+    Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
+      {
+        text: 'Sim',
+        onPress() {
+          console.warm('delete' + user.id);
+        },
+      },
+      {
+        text: 'Não',
+      },
+    ]);
+  }
+
+  function getActions(user) {
+    return (
+      <React.Fragment>
+        <Button
+          onPress={() => props.navigation.navigate('UserForm', user)}
+          type="clear"
+          icon={<Icon name="edit" size={25} color="orange" />}
+        />
+
+        <Button
+          onPress={() => confirmUserDeletion(user)}
+          type="clear"
+          icon={<Icon name="delete" size={25} color="red" />}
+        />
+      </React.Fragment>
+    );
+  }
+
   function getUserItem({ item: user }) {
     return (
       <ListItem
         key={user.id}
         bottomDivider
-        onPress={() => props.navigation.navigate('UserForm')}>
+        onPress={() => props.navigation.navigate('UserForm', user)}
+        rightContent={getActions()}>
         <Avatar source={{ uri: user.avatarUrl }} />
         <ListItem.Content>
           <ListItem.Title>{user.name}</ListItem.Title>
           <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
         </ListItem.Content>
+        <ListItem.Chevron />
       </ListItem>
     );
   }
