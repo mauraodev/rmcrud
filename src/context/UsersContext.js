3f8a1bc74568464e738b0150603f7969c47e3d84
Mauro Nunes <maurao.dev@gmail.com>
2021-08-27 00:06:57 -0300

    Add useReducer

--- src/context/UsersContext.js
+++ src/context/UsersContext.js
@@ -1,9 +1,16 @@
-import React, { createContext } from 'react';
+import React, { createContext, useReducer } from 'react';
 import users from '../data/users';
 
+const initialState = { users };
 const UsersContext = createContext({});
 
 export const UsersProvider = (props) => {
+  function reducer(state, action) {
+    console.warm(action);
+  }
+
+  useReducer(reducer, initialState);
+
   return (
     <UsersContext.Provider
       value={{
